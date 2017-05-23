0ea3c1edaa50a58bf3914ca05c29df726128a180
Nguyen Chau Chieu <nguyenchauchieu@nguyen-chau.local>
2017-05-23 22:55:27 +0700

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Nguyen Chau Chieu on 5/23/17.
+//  Copyright © 2017 Nguyen Chau Chieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
